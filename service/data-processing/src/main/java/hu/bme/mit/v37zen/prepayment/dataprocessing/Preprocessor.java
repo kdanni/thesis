0b665fd01ecc8ff3ac348fa3cff10cf337f87df1
Dániel Kiss <kissdani89@gmail.com>
2015-11-25 21:27:48 +0100

    jms

--- service/data-processing/src/main/java/hu/bme/mit/v37zen/prepayment/dataprocessing/Preprocessor.java
+++ service/data-processing/src/main/java/hu/bme/mit/v37zen/prepayment/dataprocessing/Preprocessor.java
@@ -1,5 +1,17 @@
 package hu.bme.mit.v37zen.prepayment.dataprocessing;
 
-public class Preprocessor {
+import hu.bme.mit.v37zen.sm.datamodel.smartmetering.Account;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Preprocessor {
+	
+	private static Logger logger = LoggerFactory.getLogger(Preprocessor.class);
+	
+	public void process(Account messageBody){
+		
+		logger.debug("Echo: \n" + messageBody.toString());
+		
+	}
 }
